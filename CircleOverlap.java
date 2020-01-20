d69da1cc05fba2a5f135f2a0fa864e9791579c7d
ver-tsarkova <32442706+ver-tsarkova@users.noreply.github.com>
2020-01-19 18:43:07 -0800

    Update CircleOverlap.java

--- CircleOverlap.java
+++ CircleOverlap.java
@@ -1,7 +1,5 @@
 /**
 * CircleOverlap.java
-* @author Veronica Tsarkova
-* @since 12/12/18
 * A JavaFX program that presents 20 circles (use the JavaFX Circle class), each with a random radius and location.
 * If a circle does not overlap any other circle, fill the circle with color orange. Fill overlapping circles with
 * a translucent blue (alpha value of 0.3).
